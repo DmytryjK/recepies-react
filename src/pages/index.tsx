891cbb929897fe9ac8e47b4615ca7a28c7925e55
DmytryjK <dmytryj.kyzyma@gmail.com>
2023-07-06 12:49:51 +0300

    remove unnecessary files

--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import AddRecipes from "./AddRecipes";
-import Main from "./Main";
-import AboutRecipe from "./AboutRecipe";
-import Favorites from "./Favorites";
-import AuthorizationPage from "./AuthorizationPage";
-
-export {AddRecipes, Main, AboutRecipe, Favorites, AuthorizationPage};
